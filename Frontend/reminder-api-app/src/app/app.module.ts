a2fa80b74455a675131ab957076a620388c2dde4
wesley <wesley.ti07@gmail.com>
2023-01-22 18:13:45 -0300

    criacao de componentes e service

--- Frontend/reminder-api-app/src/app/app.module.ts
+++ Frontend/reminder-api-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReminderComponent } from './reminder/reminder.component';
+import { ShowReminderComponent } from './reminder/show-reminder/show-reminder.component';
+import { AddEditReminderComponent } from './reminder/add-edit-reminder/add-edit-reminder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReminderComponent,
+    ShowReminderComponent,
+    AddEditReminderComponent
   ],
   imports: [
     BrowserModule
---
+++ Frontend/reminder-api-app/src/app/reminder/add-edit-reminder/add-edit-reminder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-reminder',
+  templateUrl: './add-edit-reminder.component.html',
+  styleUrls: ['./add-edit-reminder.component.css']
+})
+export class AddEditReminderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
