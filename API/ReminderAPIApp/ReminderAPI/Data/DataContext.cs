3eb4a21f8604dff47ef59a24b4bf83a5dd86c39d
wesley <wesley.ti07@gmail.com>
2023-01-22 17:11:05 -0300

    instalacao de pacotes e configuracao do dbcontext

---
+++ API/ReminderAPIApp/ReminderAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReminderAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+        public DbSet<Reminder> Reminders { get; set; }
+        public DbSet<ReminderType> RemindersType { get; set; }
+        public DbSet<Status> Status { get; set; }
+    }
+}
